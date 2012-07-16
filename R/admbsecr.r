b0afa4cde7f33a75cb890f6e1dc09efc4d7436ba
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-07-16 12:19:45 +0100

    Added starting values to function input paramters.

--- R/admbsecr.r
+++ R/admbsecr.r
@@ -1,6 +1,6 @@
 ## admbsecr() takes capture history and mask objects from the secr
 ## package and fits an SECR model using ADMB.
-admbsecr <- function(capt, mask, admbwd = NULL, prefix){
+admbsecr <- function(capt, mask, sv = c(0, 0.5, 1), admbwd = NULL, prefix){
   require(R2admb)
   require(secr)
   currwd <- getwd()
@@ -13,7 +13,7 @@ admbsecr <- function(capt, mask, admbwd = NULL, prefix){
   k <- dim(capt)[3]
   ## Area covered by each mask location.
   A <- attr(mask, "area")
-  ## Removing attributes from capt object as do_admb cannot handle them.
+  ## Removing attributes from capt and mask objects as do_admb cannot handle them.
   capt <- matrix(as.vector(capt), nrow = n, ncol = k)
   mask <- as.matrix(mask)
   ## No. of mask locations.
@@ -22,7 +22,7 @@ admbsecr <- function(capt, mask, admbwd = NULL, prefix){
   dist <- distances(traps, mask)
   ## Setting up parameters for do_admb.
   data <- list(n = n, ntraps = k, nmask = nm, A = A, capt = capt, dist = dist)
-  params <- list(logD = 0, g0 = 0.5, logsigma = 1)
+  params <- list(logD = sv[1], g0 = sv[2], logsigma = sv[3])
   bounds <- list(g0 = c(0, 1))
   ## Fitting the model.
   fit <- do_admb(prefix, data = data, params = params,
