9233f004aa3e3a11d516a7d75668d166141db813
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-07-20 17:03:24 +0100

    Added angles() function to helpers.

--- R/helpers.r
+++ R/helpers.r
@@ -9,6 +9,30 @@ distances <- function (X, Y) {
   t(apply(X, 1, onerow))
 }
 
+angles <- function (X, Y) {
+#-------------------------------------------------------------------------------
+# X and Y are 2-column matrices of coordinates
+# Returns angles (0,360] from points in X to points in Y 
+#         in matrix of dimension nrow(X) x nrow(Y) 
+  onerow <- function (xy) {
+    d <- function(xy2) {
+      denom=sqrt(sum((xy2-xy)^2))
+      if(denom!=0) {
+        sintheta=(xy2[1]-xy[1])/denom
+        theta=asin(sintheta)
+      } else theta=0
+      if(xy2[2]<xy[2] & xy2[1]>=xy[1]) theta=theta=pi - theta
+      if(xy2[2]<xy[2] & xy2[1]<xy[1]) theta=pi - theta
+      if(xy2[2]<xy[2] & xy2[1]==xy[1]) theta=pi
+      if(xy2[2]>=xy[2] & xy2[1]<xy[1]) theta=2*pi + theta
+      return(theta)
+    }
+    apply(Y, 1, d)
+  }
+  t(apply(X, 1, onerow))
+}
+
+
 secrlikelihood <- function (beta, capthist, mask, dist = NULL, trace = FALSE) {
   ## Compute negative log likelihood for halfnormal proximity model
   ## Murray Efford 2011-02-27
