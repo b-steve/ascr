61a3a07d6728d5239c297e24afb464d1706b0fd9
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-07-23 17:33:53 +0100

    Began implementing methods to use supplementary bearing data to fit SECR models.

--- R/admbsecr.r
+++ R/admbsecr.r
@@ -1,6 +1,6 @@
 ## admbsecr() takes capture history and mask objects from the secr
 ## package and fits an SECR model using ADMB.
-admbsecr <- function(capt, mask, sv = c(2000, 0.9, 10, 5), ssqtoa = NULL, admbwd = NULL, method = "simple"){
+admbsecr <- function(capt, traps, mask, sv = c(2000, 0.9, 10, 5), ssqtoa = NULL, angs = NULL, admbwd = NULL, method = "simple"){
   require(R2admb)
   require(secr)
   ## Warnings for incurrect input.
@@ -42,11 +42,16 @@ admbsecr <- function(capt, mask, sv = c(2000, 0.9, 10, 5), ssqtoa = NULL, admbwd
                  toassq = t(ssqtoa), dist = dist)
     params <- list(D = sv[1], g0 = sv[2], sigma = sv[3], sigmatoa = sv[4])
     bounds <- list(D = c(0, 100000), g0 = c(0, 1), sigma = c(0, 100000), sigmatoa = c(0, 100000))
+  } else if (method == "ang"){
+    ang <- angles(traps, mask)
+    data <- list(n = n, ntraps = k, nmask = nm, A = A, angcapt = capt, ang = ang, dist = dist)
+    params <- list(D = sv[1], g0 = sv[2], sigma = sv[3], kappa = sv[4])
+    bounds <- list(D = c(0, 100000), g0 = c(0, 1), sigma = c(0, 100000), kappa = c(0, 100000))
   } else {
     stop('method must be either "simple" or "toa"')
   }
   ## Adding bounds to the g0 parameter.
-
+  
   ## Fitting the model.
   fit <- do_admb(prefix, data = data, params = params, bounds = bounds, verbose = TRUE,
                  run.opts = run.control(checkdata = "write", checkparam = "write", clean = TRUE))
