da5aa86bd7fc87cef8e9d2c197cae11cddfcc91b
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-07-24 14:01:05 +0100

    Fixed bessel function so that it now calculates correctly (or at least the same as the R function besselI()).

--- ADMB/bessel.cxx
+++ ADMB/bessel.cxx
@@ -2,19 +2,20 @@
 
 dvariable bessi0(dvariable x)
 {
-  // Returns the modified Bessel function of order zero for any real x
-  // Numerical Recipes in C, 1992  
   dvariable ax,ans;
   dvariable y;
+  
   if ((ax=fabs(x)) < 3.75) {
     y=x/3.75;
     y*=y;
-    
-    ans=1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492+y*(0.2659732+y*(0.360768-1+y*0.45813e-2)))));
+    ans=1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492
+					 +y*(0.2659732+y*(0.360768e-1+y*0.45813e-2)))));
   } else {
     y=3.75/ax;
-    
-    ans=(mfexp(ax)/sqrt(ax))*(0.39894228+y*(0.1328592e-1+y*(0.225319e-2+y*(-0.157565e-2+y*(0.196281e-2+y*(-0.2057706e-1+y*(0.2635537e-1+y*(-0.1647633e-1+y*0.392377e-2))))))));
+    ans=(exp(ax)/sqrt(ax))*(0.39894228+y*(0.1328592e-1
+					  +y*(0.225319e-2+y*(-0.157565e-2+y*(0.916281e-2
+									     +y*(-0.2057706e-1+y*(0.2635537e-1+y*(-0.1647633e-1
+														  +y*0.392377e-2))))))));
   }
   return ans;
 }
