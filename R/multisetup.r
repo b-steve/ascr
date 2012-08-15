0ab52cef16185455b35cf20be0475d710a8347bc
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-08-15 12:51:05 +0100

    Altering Silvermine analysis code to run Someido too.

--- R/multisetup.r
+++ R/multisetup.r
@@ -1,10 +1,16 @@
+pref <- c("Silvermine")
+no <- 1
+
+detsname <- paste(pref, paste("dets", no, sep = ""), "csv", sep = ".")
+micsname <- paste(pref, paste("mics", no, sep = ""), "csv", sep = ".")
+
 if (.Platform$OS == "unix"){
     func.dir <- "/home/ben/SECR/R" # change this to wherever secracfuns.r and frogID.r are kept.
-    dat.dir <- "/home/ben/SECR/Data/Silvermine" # change this to wherever data are kept.
+    dat.dir <- "/home/ben/SECR/Data/Acoustic" # change this to wherever data are kept.
     admb.dir <- "/home/ben/SECR/ADMB"
 } else if (.Platform$OS == "windows") {
     func.dir <- "C:\\Documents and Settings\\Ben\\My Documents\\SECR\\R"
-    dat.dir <- "C:\\Documents and Settings\\Ben\\My Documents\\SECR\\Data\\Silvermine"
+    dat.dir <- "C:\\Documents and Settings\\Ben\\My Documents\\SECR\\Data\\Acoustic"
     admb.dir <- "C:\\Documents and Settings\\Ben\\My Documents\\SECR\\ADMB"
 }
 
@@ -17,14 +23,14 @@ source("frogID.r")
 setwd(dat.dir)
 
 ## Get and manipulate data.
-mics <- read.csv(file = "Silvermine.mics1.csv")
+mics <- read.csv(file = micsname)
 micnames <- 1:dim(mics)[1]
 mics <- cbind(micnames,mics)
 names(mics) <- c("name","x","y")
 add <- max(diff(mics$x),diff(mics$y))
 trap.xlim <- range(mics$x)+0.5*c(-add,add)
 trap.ylim <- range(mics$y)+0.5*c(-add,add)
-captures <- read.csv("Silvermine.dets1.csv")
+captures <- read.csv(file = detsname)
 traps <- read.traps(data = mics, detector = "proximity")
 border <- max(dist(traps))/4
 buffer <- border*8 # guess at buffer size - need to experiment with this
@@ -39,7 +45,12 @@ for (i in 1:length(captures$ss)){
 }
 
 ## Carry out simple SECR analysis
-ts1 <- system.time({fit = secr.fit(capt,model=list(D~1, g0~1, sigma~1),
-                    mask = mask, verify = FALSE)})
-ts2 <- system.time({fit2 = admbsecr(capt, traps = traps, mask, sv = "auto",
-                   admbwd = admb.dir, method = "simple")})
+##ts1 <- system.time({fit = secr.fit(capt,model=list(D~1, g0~1, sigma~1),
+##                    mask = mask, verify = FALSE)})
+##ts2 <- system.time({fit2 = admbsecr(capt, traps = traps, mask, sv = "auto",
+##                   admbwd = admb.dir, method = "simple")})
+
+## Carry out signal strength analysis
+##tss <- system.time({ssfit = admbsecr(capt = capt.ss, traps = traps, mask = mask,
+##                    sv = "auto",
+##                    admbwd = admb.dir, method = "ss")})
