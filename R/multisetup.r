a6bfb8973e3974e1881e5e158dae39f5a78053f0
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-09-06 13:55:47 +0100

    Analysing Silvermine and Somiedo data with new signal strength code.

--- R/multisetup.r
+++ R/multisetup.r
@@ -1,5 +1,5 @@
-pref <- c("Somiedo")
-no <- 2
+pref <- c("Silvermine")
+no <- 1
 
 detsname <- paste(pref, paste("dets", no, sep = ""), "csv", sep = ".")
 micsname <- paste(pref, paste("mics", no, sep = ""), "csv", sep = ".")
@@ -15,10 +15,13 @@ if (.Platform$OS == "unix"){
 }
 
 library(secr)
+library(Rcpp)
+library(inline)
 setwd(func.dir)
 source("admbsecr.r")
 source("helpers.r")
 source("frogID.r")
+
 
 setwd(dat.dir)
 
@@ -32,10 +35,12 @@ trap.xlim <- range(mics$x)+0.5*c(-add,add)
 trap.ylim <- range(mics$y)+0.5*c(-add,add)
 captures <- read.csv(file = detsname)
 traps <- read.traps(data = mics, detector = "proximity")
+sstraps <- read.traps(data = mics, detector = "signal")
 border <- max(dist(traps))/4
 buffer <- border*8 # guess at buffer size - need to experiment with this
 mask <- make.mask(traps, buffer = buffer, type = "trapbuffer")
 capt <- make.capthist(captures, traps, fmt = "trapID", noccasions=1)
+sscapt <- make.capthist(captures, sstraps, fmt = "trapID", noccasions = 1, cutval = 150)
 dists <- distances(traps, mask)
 
 ## Setting things up for signal strength analysis.
@@ -48,8 +53,18 @@ for (i in 1:length(captures$ss)){
 ##ts1 <- system.time({fit = secr.fit(capt,model=list(D~1, g0~1, sigma~1),
 ##                    mask = mask, verify = FALSE)})
 ##ts2 <- system.time({fit2 = admbsecr(capt, traps = traps, mask = mask, sv = "auto",
-                   admbwd = admb.dir, method = "simple", autogen = TRUE)})
+##                   admbwd = admb.dir, method = "simple", autogen = FALSE, trace = TRUE)})
 
-## Carry out signal strength analysis
-##tss2 <- system.time({ssfit2 = admbsecr(capt = capt.ss, traps = traps, mask = mask,
-##                      sv = "auto", admbwd = admb.dir, method = "ss", autogen = TRUE)})
+## Fitting signal strength analysis with nlm().
+##startval <- c(log(13000), 170, 0, log(8))
+##ssfit1 <- nlm(f = secrlikelihood.ss, p = startval, capthist = capt.ss, mask = mask,
+##              dists = dists, cutoff = 150, trace = TRUE)
+##ests <- c(exp(ssfit2$estimate[1]), ssfit2$estimate[2:3], exp(ssfit2$estimate[4]))
+## Fitting signal strength analysis with secr.fit().
+##tss1 <- system.time({ssfit = secr.fit(sscapt,model=list(D~1, g0~1, sigma~1),
+##                       detectfn = 10, mask = mask, verify = FALSE, steptol = 1e-4)})
+## Fitting signal strength analysis with admbsecr().
+##tss2 <- system.time({ssfit2 <- admbsecr(capt.ss, traps = traps, mask, sv = "auto",
+##                                        cutoff = 150, admbwd = admb.dir,
+##                                        method = "ss", clean = TRUE, trace = TRUE, 
+##                                        autogen = FALSE)})
