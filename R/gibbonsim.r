2bda2cf8df88011e0ff12b5b3e5274996b3addf2
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-09-11 12:25:06 +0100

    Forgot to wrap model fitting with try().

--- R/gibbonsim.r
+++ R/gibbonsim.r
@@ -2,6 +2,7 @@ library("secr")
 library("CircStats")
 library("inline")
 library("Rcpp")
+library("R2adb")
 
 if (.Platform$OS == "unix"){
     source("/home/ben/SECR/R/helpers.r")
@@ -17,7 +18,7 @@ if (.Platform$OS == "unix"){
     dat.dir <- "C:\\Documents and Settings\\Ben\\My Documents\\SECR\\Data\\Gibbons\\gibbons.txt"
 }
 
-nsims <- 4000
+nsims <- 1000
 buffer <- 6000
 mask.spacing <- 100
 trap.spacing <- 2500
@@ -39,13 +40,16 @@ A <- attr(mask, "area")
 mask.dists <- distances.cpp(as.matrix(traps), as.matrix(mask))
 mask.angs <- angles.cpp(as.matrix(traps), as.matrix(mask))
 
+simprobs <- NULL
+angprobs <- NULL
 simpleres <- matrix(0, nrow = nsims, ncol = 3)
 angres <- matrix(0, nrow = nsims, ncol = 4)
 colnames(simpleres) <- c("D", "g0", "sigma")
 colnames(angres) <- c("D", "g0", "sigma", "kappa")
-print(c("start", date()))
 for (i in 1:nsims){
-  if (i %% 100 == 0){
+  if (i == 1){
+    print(c("start", date()))
+  } else if (i %% 100 == 0){
     print(c(i, date()))
   }
   ## Simulating data and setting things up for analysis
@@ -63,23 +67,40 @@ for (i in 1:nsims){
   radians[radians == 0] <- 2*pi
   radhist <- capthist
   radhist[radhist == 1] <- radians[radhist == 1]
-  
-  p <- c(log(D), invlogit(g0), log(sigma))
-  ## Straightforward secr model using admbsecr()
-  simplefit <- admbsecr(capt = capthist, traps = traps, mask = mask, sv = truepars[1:3],
-                        admbwd = admb.dir, method = "simple", verbose = FALSE, autogen = FALSE)
-  angfit <- admbsecr(capt = radhist, traps = traps, mask = mask, sv = truepars,
+  ## Straightforward SECR model using admbsecr()
+  simplefit <- try(admbsecr(capt = capthist, traps = traps, mask = mask, sv = truepars[1:3],
+                        admbwd = admb.dir, method = "simple", verbose = FALSE, autogen = FALSE),
+                   silent = TRUE)
+  if (class(simplefit) == "try-error"){
+    simplefit <- try(admbsecr(capt = capthist, traps = traps, mask = mask, sv = "auto",
+                              admbwd = admb.dir, method = "simple", verbose = FALSE,
+                              autogen = FALSE), silent = TRUE)
+  }
+  if (class(simplefit) == "try-error"){
+    simplecoef <- NA
+    simprobs <- c(simprobs, i)
+  } else {
+    simplecoef <- coef(simplefit)
+  }
+  ## SECR model using supplementary angle data
+  angfit <- try(admbsecr(capt = radhist, traps = traps, mask = mask, sv = truepars,
                      angs = mask.angs, admbwd = admb.dir, method = "ang", verbose = FALSE,
-                     autogen = FALSE)
-  simpleres[i, ] <- coef(simplefit)
-  angres[i, ] <- coef(angfit)
+                     autogen = FALSE), silent = TRUE)
+  if (class(angfit) == "try-error"){
+    angfit <- try(admbsecr(capt = radhist, traps = traps, mask = mask, sv = "auto",
+                       angs = mask.angs, admbwd = admb.dir, method = "ang", verbose = FALSE,
+                       autogen = FALSE), silent = TRUE)
+  }
+  if (class(angfit) == "try-error"){
+    angcoef <- NA
+    angprobs <- c(angprobs, i)
+  } else {
+    angcoef <- coef(angfit)
+  }
+  simpleres[i, ] <- simplecoef
+  angres[i, ] <- angcoef
+  if (i == nsims){
+    print(c("end", date()))
+  }
 }
-print(c("end", date()))
 
-
-plot(simpleres[, 1], rep(1, 10), ylim = c(0, 3))
-points(angres[, 1], rep(2, 10))
-abline(v = truepars[1], lty = "dotted")
-for (i in 1:10){
-  lines(x = c(simpleres[i, 1], angres[i, 1]), y = 1:2)
-}
