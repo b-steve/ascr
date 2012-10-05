ec70cf1334edcc476d163261b7fcb9cfe92aa31e
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-10-05 09:53:50 +0100

    Adding code to create comparison plot.

--- R/gibbonsim.r
+++ R/gibbonsim.r
@@ -126,3 +126,42 @@ abline(v = D, lty = "dotted")
 
 ##write.table(angres, "/home/ben/SECR/Results/3/angres.txt", row.names = FALSE)
 ##write.table(simpleres, "/home/ben/SECR/Results/3/simpleres.txt", row.names = FALSE)
+
+## Folder containing angres.txt, simres.txt, pars.r.
+resfile <- "/home/ben/SECR/Results/3/"
+source(paste(resfile, "pars.r", sep = ""))
+angres <- read.table(paste(resfile, "angres.txt", sep = ""), header = TRUE)
+simpleres <- read.table(paste(resfile, "simpleres.txt", sep = ""), header = TRUE)
+
+## Assigning the columns to vectors.
+for (i in colnames(simpleres)){
+  name <- paste("sim", i, sep = "")
+  assign(name, simpleres[, i])
+}
+for (i in colnames(angres)){
+  name <- paste("ang", i, sep = "")
+  assign(name, angres[, i])
+}
+
+## Two different bandwidth selections.
+##dsimD <- density(simD)
+##dangD <- density(angD)
+dsimD <- density(simD, bw = "bcv")
+dangD <- density(angD, bw = "bcv")
+xs <- c(dsimD$x, dangD$x)
+ys <- c(dsimD$y, dangD$y)
+
+
+pdf(file = paste(resfile, "fig", sep = ""))
+plot.new()
+plot.window(xlim = range(xs), ylim = c(0, max(ys)))
+axis(1)
+axis(2, las = 1)
+abline(v = D, lty = "dotted")
+lines(dsimD, col = "blue")
+lines(dangD, col = "red")
+abline(h = 0, col = "grey")
+box()
+title(main = "Simulated sampling distributions of animal density", xlab = expression(hat(D)), ylab = "Density")
+legend(x = "topright", legend = c("SECR", "SECR + angles"), col = c("blue", "red"), lty = 1)
+dev.off()
