377c8274c17d97ee9301592d70faf7f66beef905
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-11-01 13:53:03 +0000

    Fixing parameters file.

--- Results/whales/1/pars.r
+++ Results/whales/1/pars.r
@@ -1,11 +1,9 @@
-## setup for simulations.
-nsims <- 500
-buffer <- 3000
-mask.spacing <- 45
-
-## True parameter values.
 seed <- 6843
 D <- 7.10
 g0 <- 0.29
 sigma <- 245
 alpha <- 5.6
+buffer <- 3000
+mask.spacing <- 45
+truepars <- c(D = D, g0 = g0, sigma = sigma, alpha = alpha)
+detectpars <- list(g0 = g0, sigma = sigma)
