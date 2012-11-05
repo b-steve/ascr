58923253194ba2c52302190f85975f133b0bb1f8
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-11-05 17:30:21 +0000

    Working version of code for signal strength models after change to exponential link function.

--- Analysis/frogs.r
+++ Analysis/frogs.r
@@ -41,12 +41,13 @@ toafit1 <- admbsecr(capt = capt.toa, traps = traps, mask = mask, sv = "auto",
 ssfit1 <- secr.fit(sscapt, model = list(D~1, g0~1, sigma~1), detectfn = 10, mask = mask,
                    verify = FALSE, steptol = 1e-4)
 ## Fitting with admbsecr().
-ssfit2 <- admbsecr(capt.ss, traps = traps, mask, sv = "auto", cutoff = 150,
-                   admbwd = admb.dir, method = "ss", autogen = FALSE, trace = TRUE)
+ssfit2 <- admbsecr(capt.ss, traps = traps, mask, cutoff = 150, sv = "auto",
+                   admbwd = admb.dir, method = "ss", autogen = FALSE)
 
 ## Carrying out analysis with both signal strength and TOA information incorporated.
 ## Only possible with admbsecr().
-sv <- c(coef(toafit1)[c(1, 4)], coef(ssfit2)[2:4])
-jointfit <- admbsecr(capt = capt.joint, traps = traps, mask = mask, sv = sv,
+##sv <- c(coef(toafit1)[c(1, 4)], coef(ssfit2)[2:4])
+jointfit <- admbsecr(capt = capt.joint, traps = traps, mask = mask,
+                     bounds = list(D = c(0, 1e5), ssb1 = c(-1, 0)),
                      cutoff = 150, admbwd = admb.dir, method = "sstoa",
                      autogen = FALSE, trace = TRUE)
