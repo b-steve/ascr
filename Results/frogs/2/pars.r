ae3d1f8e711622f2dc046e47cf85d3a35a7685b8
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-11-07 15:56:24 +0000

    Adding directory for simulation results.

---
+++ Results/frogs/2/pars.r
@@ -0,0 +1,15 @@
+## Setup for simulations.
+nsims <- 1
+buffer <- 35
+mask.spacing <- 50
+
+## True parameter values.
+seed <- 9578
+D <- 5270
+g0 <- 0.99999
+sigma <- 5.60
+sigmatoa <- 0.002
+ssb0 <- 5.16
+ssb1 <- -0.02
+sigmass <- 6.20
+cutoff <- 150
