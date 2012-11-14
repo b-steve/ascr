b708945910367341c4cfadde4857b2c5f4fd352d
Ben Stevenson <bcs5@st-andrews.ac.uk>
2012-11-14 17:00:26 +0000

    Setting up simulation code for dist vs mrds comparison.

--- Analysis/whalesim.r
+++ Analysis/whalesim.r
@@ -9,9 +9,8 @@ admb.dir <- paste(admbsecr.dir, "ADMB", sep = sep)
 work.dir <- paste(admbsecr.dir, "Analysis", sep = sep)
 dat.dir <- paste(admbsecr.dir, "Data", sep = sep)
 
-## Get required libraries.
+## Get required library.
 library(secr)
-library(plyr)
 
 ## Loading the admbsecr library.
 setwd(admbsecr.dir)
@@ -29,38 +28,36 @@ options(warn = 1)
 
 setwd(work.dir)
 ## setup for simulations.
-nsims <- 500
+nsims <- 2
 buffer <- 3000
 mask.spacing <- 45
 
 ## True parameter values.
-seed <- 6843
-D <- 7.10
-g0 <- 0.29
-sigma <- 245
-alpha <- 5.6
-truepars <- c(D = D, g0 = g0, sigma = sigma, alpha = alpha)
-detectpars <- list(g0 = g0, sigma = sigma)
+seed <- 8465
+D <- 1.7
+g01 <- 0.99999
+sigma1 <- 210
+g02 <- 0.30
+sigma2 <- 320
+alpha <- 26
+truepars <- c(D = D, g01 = g01, sigma1 = sigma1, g02 = g02,
+              sigma2 = sigma2, alpha = alpha)
+detectpars <- list(g01 = g01, sigma1 = sigma1, g02 = g02,
+                   sigma2 = sigma2, alpha = alpha)
 
 set.seed(seed)
 
-## Setting up mask and traps.
-ntraps <- nrow(traps)
+## Setting up mask.
 mask <- make.mask(fake.traps, buffer = buffer, spacing = mask.spacing,
                   type = "trapbuffer")
-nmask <- nrow(mask)
-A <- attr(mask, "area")
-mask.dists <- distances(as.matrix(real.traps), as.matrix(mask))
-probs <- NULL
-res <- matrix(0, nrow = nsims, ncol = 4)
-colnames(res) <- c("D", "g0", "sigma", "alpha")
 
-## Function for generating random gamma values:
-estdists <- function(x, alpha){
-  betas <- alpha/x
-  c(rgamma(1, shape = alpha, rate = betas[1]),
-    rgamma(1, shape = alpha, rate = betas[2]))
-}
+## Results matrices.
+distres <- matrix(0, nrow = nsims, ncol = 6)
+colnames(distres) <- c("D", "g01", "sigma1", "g02", "sigma2", "alpha")
+mrdsres <- matrix(0, nrow = nsims, ncol = 5)
+colnames(mrdsres) <- c("D", "g01", "sigma1", "g02", "sigma2")
+distprobs <- NULL
+mrdsprobs <- NULL
 
 ## Carrying out simulation.
 for (i in 1:nsims){
@@ -71,62 +68,66 @@ for (i in 1:nsims){
   }
   ## Simulating data and setting things up for analysis.
   popn <- sim.popn(D = D, core = real.traps, buffer = buffer)
-
-  capthist <- sim.capthist(real.traps, popn, detectfn = 0,
-                           detectpar = detectpars, noccasions = 1,
-                           renumber = FALSE)
-  n <- nrow(capthist)
-  ndets <- sum(capthist)
-  cue.ids <- unique(as.numeric(rownames(capthist)))
-  detections <- popn[cue.ids, ]
-  dists <- distances(as.matrix(detections), as.matrix(real.traps))
-  dists <- aaply(dists, 1, estdists, alpha = alpha)
-  dists <- array(dists, dim = c(dim(dists)[1], 1, dim(dists)[2]))
-  capthist.dist <- as.array(dists*capthist)
-  fit <- try(admbsecr(capthist.dist, traps = real.traps, mask = mask,
-                  sv = c(D = D, g0 = g0, sigma = sigma, alpha = alpha),
-                  admbwd = admb.dir, method = "dist", autogen = FALSE),
-             silent = TRUE)
-  if (class(fit) == "try-error"){
-    coef <- NA
-    probs <- c(probs, i)
+  capthists <- sim.capthist.dist(real.traps, popn, detectpars)
+  capthist.dist <- capthists$dist
+  capthist.mrds <- capthists$mrds
+  distfit <- disttrapcov(capt = capthist.dist, mask = mask, traps = real.traps,
+                         sv = c(D, g01, sigma1, g02, sigma2, alpha),
+                         admb.dir = admb.dir, clean = TRUE,
+                         verbose = FALSE, trace = FALSE)
+  mrdsfit <- mrdstrapcov(capt = capthist.mrds, mask = mask, traps = real.traps,
+                         sv = c(D, g01, sigma1, g02, sigma2), admb.dir = admb.dir,
+                         clean =  TRUE, verbose = FALSE, trace = FALSE)
+  if (class(distfit) == "try-error"){
+    distcoef <- NA
+    distprobs <- c(distprobs, i)
+  } else {
+    distcoef <- coef(distfit)
   }
-  coef <- coef(fit)
-  res[i, ] <- coef
+  if (class(mrdsfit) == "try-error"){
+    mrdscoef <- NA
+    mrdsprobs <- c(mrdsprobs, i)
+  } else {
+    mrdscoef <- coef(mrdsfit)
+  }
+  distres[i, ] <- distcoef
+  mrdsres[i, ] <- mrdscoef
 }
 
 ## To write the simulation results to a file.
-##respath <- paste(admbsecr.dir, "Results/whales/1/res.txt", sep = "/")
-##write.table(res, respath, row.names = FALSE)
+distrespath <- paste(admbsecr.dir, "Results/whales/2/distres.txt", sep = "/")
+mrdsrespath <- paste(admbsecr.dir, "Results/whales/2/mrdsres.txt", sep = "/")
+write.table(distres, distrespath, row.names = FALSE)
+write.table(mrdsres, mrdsrespath, row.names = FALSE)
 
-## To read in simulation results from a file.
-resfile <- "~/admbsecr/Results/whales/1/"
-source(paste(resfile, "pars.r", sep = ""))
-res <- read.table(paste(resfile, "res.txt", sep = ""), header = TRUE)
+## ## To read in simulation results from a file.
+## resfile <- "~/admbsecr/Results/whales/1/"
+## source(paste(resfile, "pars.r", sep = ""))
+## res <- read.table(paste(resfile, "res.txt", sep = ""), header = TRUE)
 
-## Assigning the columns to vectors.
-for (i in colnames(res)){
-  name <- paste("res", i, sep = "")
-  assign(name, res[, i])
-}
+## ## Assigning the columns to vectors.
+## for (i in colnames(res)){
+##   name <- paste("res", i, sep = "")
+##   assign(name, res[, i])
+## }
 
-## Calculating densities.
-dD <- density(resD)
-dg0 <- density(resg0)
-dsigma <- density(ressigma)
-dalpha <- density(resalpha)
-xs <- dD$x
-ys <- dD$y
+## ## Calculating densities.
+## dD <- density(resD)
+## dg0 <- density(resg0)
+## dsigma <- density(ressigma)
+## dalpha <- density(resalpha)
+## xs <- dD$x
+## ys <- dD$y
 
-##pdf(file = paste(resfile, "fig", sep = ""))
-plot.new()
-plot.window(xlim = range(xs), ylim = c(0, max(ys)))
-axis(1)
-axis(2, las = 1)
-abline(v = D, lty = "dotted")
-lines(dD, col = "blue")
-abline(h = 0, col = "grey")
-box()
-title(main = "Simulated sampling distributions of animal density",
-      xlab = expression(hat(D)), ylab = "Density")
-##dev.off()
+## ##pdf(file = paste(resfile, "fig", sep = ""))
+## plot.new()
+## plot.window(xlim = range(xs), ylim = c(0, max(ys)))
+## axis(1)
+## axis(2, las = 1)
+## abline(v = D, lty = "dotted")
+## lines(dD, col = "blue")
+## abline(h = 0, col = "grey")
+## box()
+## title(main = "Simulated sampling distributions of animal density",
+##       xlab = expression(hat(D)), ylab = "Density")
+## ##dev.off()
