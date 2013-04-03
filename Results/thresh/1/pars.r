ebadb7db82ac16f573b82c2e66bcc12337b19d01
Ben Stevenson <bcs5@st-andrews.ac.uk>
2013-04-03 18:06:41 +0100

    Adding pars.r file.

---
+++ Results/thresh/1/pars.r
@@ -0,0 +1,9 @@
+## Simulating with signal strength and testing with a bunch of
+## detection functions. There were fitting problems with (at least)
+## the logth method.
+seed <- 9578
+D <- 5270
+ssb0 <- 173
+ssb1 <- -3.1
+sigmass <- 6.20
+cutoff <- 150
