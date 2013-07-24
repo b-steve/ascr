507c0e680dd7700256f1b634140a5bf85f3bb926
b-steve <bcs5@st-andrews.ac.uk>
2013-07-24 14:07:58 +0200

    Making pars.r sustainable.

--- Results/secorrect/1/pars.r
+++ Results/secorrect/1/pars.r
@@ -1,3 +1,5 @@
+wd <- getwd()
+
 ## Setup for simulations.
 seed <- 15338
 set.seed(seed)
@@ -19,6 +21,12 @@ bounds <- list(D = c(0, 20000), sigma <- c(0, 30))
 scalefactors <- c(shape = 1000, scale = 1000)
 
 ## Setting up mask and traps.
+setwd("~/admbsecr/Data")
+mics <- read.csv(file = "array1a-top.csv")
+micnames <- 1:dim(mics)[1]
+mics <- cbind(micnames, mics)
+traps <- read.traps(data = mics, detector = "signal")
+setwd(wd)
 ntraps <- nrow(traps)
 mask <- make.mask(traps, buffer = buffer, type = "trapbuffer")
 nmask <- nrow(mask)
