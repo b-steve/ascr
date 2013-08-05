9953c4379504b082267397363a1eaaf9bcd824f9
Ben Stevenson <bcs5@st-andrews.ac.uk>
2013-08-05 17:16:36 +0100

    Altering analysis code for full set of simulations.

--- Analysis/secorrectplot.r
+++ Analysis/secorrectplot.r
@@ -11,29 +11,27 @@ setwd("~/admbsecr/Results/secorrect/1/fits")
 fit.files <- list.files()
 
 nfits <- length(fit.files)
-Ds <- NULL
-Ds.c <- NULL
-ses <- NULL
-ses.c <- NULL
-work.ids <- NULL
+
+Ds <- numeric(500)
+Ds.c <- numeric(500)
+ses <- numeric(500)
+ses.c <- numeric(500)
+boots.l <- list()
+
 j <- 1
 for (i in fit.files){
   load(i)
-  if (class(out$fit)[1] != "try-error"){
-    work.ids <- c(work.ids, i)
-    boots <- out$fit.sec$se.correct$boots
-    coefs <- out$fit$coefficients
-    bias <- apply(boots, 2, mean, na.rm = TRUE) - coefs
-    out$fit.sec$se.correct$coefficients.corrected <- coefs - bias
-    out$fit.sec$se.correct$se.corrected <- apply(boots, 2, sd, na.rm = TRUE)
-    Ds <- c(Ds, out$fit$coefficients[1])
-    Ds.c <- c(Ds.c, out$fit.sec$se.correct$coefficients.corrected[1])
-    ses <- c(ses, out$fit$se[1])
-    ses.c <- c(ses.c, out$fit.sec$se.correct$se.corrected[1])
-    j <- j + 1
-  } else {
-    file.remove(i)
-  }
+  boots <- out$fit.sec$se.correct$boots
+  coefs <- out$fit$coefficients
+  bias <- apply(boots, 2, mean, na.rm = TRUE) - coefs
+  out$fit.sec$se.correct$coefficients.corrected <- coefs - bias
+  out$fit.sec$se.correct$se.corrected <- apply(boots, 2, sd, na.rm = TRUE)
+  Ds[j] <- out$fit$coefficients[1]
+  Ds.c[j] <- out$fit.sec$se.correct$coefficients.corrected[1]
+  ses[j] <- out$fit$se[1]
+  ses.c[j] <- out$fit.sec$se.correct$se.corrected[1]
+  boots.l[[j]] <- boots
+  j <- j + 1
 }
 
 mean(Ds)
@@ -41,10 +39,13 @@ mean(Ds.c)
 mean(ses, na.rm = TRUE)
 mean(ses.c)
 
-Ds.c <- Ds.c[!is.na(Ds.c)]
-
-boots <- out$fit.sec$se.correct$boots
-coefs <- out$fit$coefficients
-bias <- apply(boots, 2, mean, na.rm = TRUE) - coefs
-out.fitsec$secorrect$coefficients.corrected <- coefs - bias
-out$fitsec$secorrect$se.corrected <- apply(boots, 2, sd, na.rm = TRUE)
+cis.norm <- matrix(0, nrow = 500, ncol = 2)
+cis.perc <- matrix(0, nrow = 500, ncol = 2)
+for (i in 1:500){
+  cis.norm[i, ] <- Ds.c[i] + c(-1, 1)*qnorm(0.975)*ses.c[i]
+  cis.perc[i, ] <- quantile(boots.l[[i]][, 1], c(0.025, 0.975), na.rm = TRUE)
+}
+cov.norm <- apply(cis.norm, 1, function(x) x[1] <= 1750 & x[2] >= 1750)
+mean(cov.norm)
+cov.perc <- apply(cis.perc, 1, function(x) x[1] <= 1750 & x[2] >= 1750)
+mean(cov.perc)
