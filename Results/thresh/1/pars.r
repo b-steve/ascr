b0c4da163f011139740230691dad3404e3d90f96
Ben Stevenson <bcs5@st-andrews.ac.uk>
2013-09-19 16:01:20 +0100

    Removing files moved to sawpd repo.

--- Results/thresh/1/pars.r
+++
@@ -1,9 +0,0 @@
-## Simulating with signal strength and testing with a bunch of
-## detection functions. There were fitting problems with (at least)
-## the logth method.
-seed <- 6846
-D <- 5270
-ssb0 <- 173
-ssb1 <- -3.1
-sigmass <- 6.20
-cutoff <- 150
