c18d1701fdf34307cd0d2ec70c695c32cd27f45e
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-01-30 01:34:14 +0000

    Adding tests for start values, fixed parameters, and bounds (latter unfinished).

--- inst/tests/test-simple.r
+++ inst/tests/test-simple.r
@@ -35,4 +35,43 @@ test_that("simple fitting", {
     expect_that(relative.error < 1e-4, is_true())
 })
 
+test_that("fixing parameters", {
+    simple.capt <- example.capt["bincapt"]
+    fit <- admbsecr(capt = simple.capt, traps = example.traps,
+                    mask = example.mask, fix = list(g0 = 0.9))
+    ## Checking that g0 is not estimated.
+    expect_that(getpar(fit, "g0"), is_equivalent_to(0.9))
+    ## Checkint that phase is set to -1.
+    expect_that(fit$phases$g0, is_equivalent_to(-1))
+    ## Checking that all parameters have a phase.
+    all.pars <- c("D", fit$detpars, fit$suppars)
+    phase.pars <- names(fit$phases)
+    expect_that(sort(phase.pars), equals(sort(all.pars)))
+    ## Checking that no other parameters are set to -1.
+    active.phases <- c(fit$phases[phase.pars != "g0"],
+                       recursive = TRUE)
+    expect_that(all(active.phases > -1), is_true())
+    
+})
 
+test_that("start values", {
+    simple.capt <- example.capt["bincapt"]
+    ## Fit original model.
+    fit.start <- admbsecr(capt = simple.capt, traps = example.traps,
+                          mask = example.mask)
+    ## Provide a single start value.{
+    fit <- admbsecr(capt = simple.capt, traps = example.traps,
+                    mask = example.mask, sv = list(D = 2145))
+    ## Check that estimates are the same.
+    relative.error <- max(abs((coef(fit.start) - coef(fit))/
+                              coef(fit)))
+    expect_that(relative.error < 1e-4, is_true())
+    ## Check start value is passed correctly.
+    expect_that(fit$sv$D, is_equivalent_to(2145))
+})
+
+test_that("parameter bounds", {
+    simple.capt <- example.capt["bincapt"]
+    fit <- admbsecr(capt = simple.capt, traps = example.traps,
+                    mask = example.mask, sv = list(D = 2145))
+})
