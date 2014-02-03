8f00504283388f83f13576aa3c621a0ac0440276
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-02-03 09:22:40 +0000

    Splitting sim.capt() and detection functions away from helper file.

---
+++ R/detfns.r
@@ -0,0 +1,65 @@
+## Returns a detection function.
+get.detfn <- function(detfn){
+    switch(detfn, hn = calc.hn, hr = calc.hr, th = calc.th,
+           lth = calc.lth, ss = calc.ss, log.ss = calc.log.ss)
+}
+
+calc.hn <- function(d, pars){
+    if (any(sort(pars) != c("g0", "sigma"))){
+        stop("Argument 'pars' must have named components 'g0' and 'sigma'.")
+    }
+    g0 <- pars$g0
+    sigma <- pars$sigma
+    g0*exp(-(d^2/(2*sigma^2)))
+}
+
+calc.hr <- function(d, pars){
+    if (any(sort(pars) != c("g0", "sigma", "z"))){
+        stop("Argument 'pars' must have named components 'g0', 'sigma', and 'z'.")
+    }
+    g0 <- pars$g0
+    sigma <- pars$sigma
+    z <- pars$z
+    g0*(1 - exp(-((d/sigma)^-z)))
+}
+
+calc.th <- function(d, pars){
+    if (any(sort(pars) != c("scale", "shape"))){
+        stop("Argument 'pars' must have named components 'scale' and 'shape'.")
+    }
+    scale <- pars$scale
+    shape <- pars$shape
+    0.5 - 0.5*erf(d/scale - shape)
+}
+
+calc.lth <- function(d, pars){
+    if (any(sort(pars) != c("scale", "shape.1", "shape.2"))){
+        stop("Argument 'pars' must have named components 'scale', 'shape.1', and 'shape.2'.")
+    }
+    scale <- pars$scale
+    shape.1 <- pars$shape.1
+    shape.2 <- pars$shape.2
+    0.5 - 0.5*erf(shape.1 - exp(shape.2 + scale*d))
+}
+
+calc.ss <- function(d, pars){
+    if (any(sort(pars) != c("b0.ss", "b1.ss", "cutoff", "sigma.ss"))){
+        stop("Argument 'pars' must have named components 'b0.ss', 'b1.ss', 'sigma.ss', and 'cutoff'.")
+    }
+    b0.ss <- pars$b0.ss
+    b1.ss <- pars$b1.ss
+    sigma.ss <- pars$sigma.ss
+    cutoff <- pars$cutoff
+    1 - pnorm(cutoff, mean = b0.ss - b1.ss*d, sd = sigma.ss)
+}
+
+calc.log.ss <- function(d, pars){
+    if (any(sort(pars) != c("b0.ss", "b1.ss", "cutoff", "sigma.ss"))){
+        stop("Argument 'pars' must have named components 'b0.ss', 'b1.ss', 'sigma.ss', and 'cutoff'.")
+    }
+    b0.ss <- pars$b0.ss
+    b1.ss <- pars$b1.ss
+    sigma.ss <- pars$sigma.ss
+    cutoff <- pars$cutoff
+    1 - pnorm(cutoff, mean = exp(b0.ss - b1.ss*d), sd = sigma.ss)
+}
