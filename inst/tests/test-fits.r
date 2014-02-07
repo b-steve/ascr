f2e30050d382b80249627dfb487c1e60269257a6
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-02-07 13:21:57 +0000

    Adding test for a hazard rate fit.

--- inst/tests/test-fits.r
+++ inst/tests/test-fits.r
@@ -1,6 +1,6 @@
 context("Testing model fits")
 
-test_that("simple fitting", {
+test_that("simple fitting -- half normal", {
     ## Fitting model.
     simple.capt <- example.capt["bincapt"]
     fit <- admbsecr(capt = simple.capt, traps = example.traps,
@@ -24,6 +24,42 @@ test_that("simple fitting", {
     capt.secr <- convert.capt(example.capt, example.traps)
     options(warn = -1)
     fit.secr <- secr.fit(capthist = capt.secr, mask = mask.secr, trace = FALSE)
+    options(warn = 0)
+    coefs.secr <- numeric(n.pars)
+    invlog <- function(x) exp(x)
+    for (i in 1:n.pars){
+        coefs.secr[i] <- eval(call(paste("inv", fit.secr$link[[i]], sep = ""),
+                                   fit.secr$fit$par[i]))
+    }
+    relative.error <- max(abs((coef(fit) - coefs.secr)/coefs.secr))
+    expect_that(relative.error < 1e-4, is_true())
+})
+
+test_that("simple fitting -- hazard rate", {
+    ## Fitting model.
+    simple.capt <- example.capt["bincapt"]
+    fit <- admbsecr(capt = simple.capt, traps = example.traps,
+                    mask = example.mask, detfn = "hr")
+    ## Checking parameter values.
+    pars.test <- c(2601.486636, 0.9283243400, 6.782970400, 6.409336100)
+    n.pars <- length(pars.test)
+    relative.error <- max(abs((coef(fit) - pars.test)/pars.test))
+    expect_that(relative.error < 1e-4, is_true())
+    ## Checking standard errors.
+    ses.test <- c(348.14, 0.074121, 0.71436, 1.4893)
+    relative.error <- max(abs((stdEr(fit) - ses.test)/ses.test))
+    expect_that(relative.error < 1e-4, is_true())
+    ## Checking detection parameters.
+    expect_that(fit$detpars, equals(c("g0", "sigma", "z")))
+    ## Checking supplementary parameters.
+    expect_that(length(fit$suppars), equals(0))
+    ## Checking estimate equivalence with secr.fit.
+    library(secr)
+    mask.secr <- convert.mask(example.mask)
+    capt.secr <- convert.capt(example.capt, example.traps)
+    options(warn = -1)
+    fit.secr <- secr.fit(capthist = capt.secr, mask = mask.secr, detectfn = 1,
+                         trace = FALSE)
     options(warn = 0)
     coefs.secr <- numeric(n.pars)
     invlog <- function(x) exp(x)
