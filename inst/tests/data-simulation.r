e5e5585d1c53aefa6aef923b22b943a5479b1c06
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-03-10 11:11:39 +0000

    Adding some model fits to data.

--- inst/tests/data-simulation.r
+++ inst/tests/data-simulation.r
@@ -1,9 +1,18 @@
 set.seed(8173)
 example.capt <- sim.capt(traps = example.traps, mask = example.mask,
-                         infotypes = c("ang", "dist", "toa"),
+                         infotypes = c("bearing", "dist", "toa"),
                          detfn = "ss",
                          pars = list(D = 2500, b0.ss = 90, b1.ss = 4,
                              sigma.ss = 10, kappa = 70, alpha = 4,
-                             sigma.toa = 0.002), cutoff = 60)    
-##save(example.traps, example.mask, example.capt,
-##     file = "~/admbsecr/data/simdata.RData")
+                             sigma.toa = 0.002), cutoff = 60)
+simple.capt <- example.capt["bincapt"]
+bearing.capt <- example.capt[c("bincapt", "bearing")]
+simple.hn.fit <- admbsecr(capt = simple.capt, traps = example.traps,
+                          mask = example.mask, fix = list(g0 = 1))
+simple.hr.fit <- admbsecr(capt = simple.capt, traps = example.traps,
+                          mask = example.mask, detfn = "hr")
+bearing.hn.fit <- admbsecr(capt = bearing.capt, traps = example.traps,
+                           mask = example.mask, fix = list(g0 = 1))
+save(example.traps, example.mask, example.capt, simple.hn.fit,
+     simple.hr.fit, bearing.hn.fit,
+     file = "/scratch/admbsecr/data/simdata.RData")
