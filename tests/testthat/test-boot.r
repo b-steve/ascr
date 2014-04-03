c63c033ee480553738f4359da479022d89c708d3
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-04-03 16:24:12 +0100

    Fixing location of bootstrapped estimates.

--- tests/testthat/test-boot.r
+++ tests/testthat/test-boot.r
@@ -4,7 +4,7 @@ test_that("simple model bootstrapping", {
     set.seed(8871)
     boot.fit <- boot.admbsecr(simple.hn.fit, N = 5, prog = FALSE)
     means.test <- c(7.6667469, 1.7383131, 2165.68, 5.705, 0.0616552)
-    boot.means <- apply(boot.fit$boot, 2, mean)
+    boot.means <- apply(boot.fit$boot$boots, 2, mean)
     relative.error <- max(abs((boot.means - means.test)/means.test))
     expect_that(relative.error < 1e-4, is_true())
     expect_that(coef(boot.fit), is_identical_to(coef(simple.hn.fit)))
