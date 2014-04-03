52c9870a219f656094cfce71c2b15f9357872cf5
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-04-03 16:23:41 +0100

    Adding extra model object.

--- inst/data-simulation.r
+++ inst/data-simulation.r
@@ -13,7 +13,8 @@ simple.hr.fit <- admbsecr(capt = simple.capt, traps = example.traps,
                           mask = example.mask, detfn = "hr")
 bearing.hn.fit <- admbsecr(capt = bearing.capt, traps = example.traps,
                            mask = example.mask, fix = list(g0 = 1))
+boot.simple.hn.fit <- boot.admbsecr(simple.hn.fit, N = 500, n.cores = 4)
 ##save(example.traps, example.mask, example.capt,
 ##     file = "/scratch/admbsecr/data/example_data.RData")
-##save(simple.hn.fit, simple.hr.fit, bearing.hn.fit,
+##save(simple.hn.fit, simple.hr.fit, bearing.hn.fit, boot.simple.hn.fit,
 ##     file = "/scratch/admbsecr/data/example_fits.RData")
