4c152b66ef5692da827b9091bca117b7be15c172
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-04-09 14:04:27 +0100

    Some updates.

--- R/admbsecr.r
+++ R/admbsecr.r
@@ -770,11 +770,12 @@ admbsecr <- function(capt, traps, mask, detfn = "hn", sv = NULL, bounds = NULL,
             out[["vcov"]] <- vcov.updated
         }
     }
+    y <- 3
     out$fit.freqs <- fit.freqs
     if (out$maxgrad < -0.01){
         warning("Maximum gradient component is large.")
     }
-    x <- 3
+    x <- 2
     class(out) <- c("admbsecr", "admb")
     out
 }
