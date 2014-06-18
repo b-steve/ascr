2e1f78adbeac8ee8834b86fb3205b516421b4a0a
Ben Stevenson <bcs5@st-andrews.ac.uk>
2014-06-18 14:39:33 +0100

    Updating installation script.

--- install.r
+++ install.r
@@ -1,6 +1,25 @@
 install.packages(c("CircStats", "lattice", "matrixStats", "plyr",
                    "Rcpp", "R2admb", "secr", "testthat", "downloader"))
+if (.Platform$OS == "windows"){
+    os <- "win"
+    ext <- ".zip"
+} else if (.Platform$OS == "unix"){
+    if (Sys.info()["sysname"] == "Linux"){
+        os <- "linux"
+        ext <- ".tar.gz"
+    } else if (Sys.info()["sysname"] == "Darwin"){
+        os <- "osx"
+        suff <- ".tgz"
+    } else {
+        stop("Unknown OS type.")
+    }
+} else {
+    stop("Unknown OS type.")
+}
+bin.name <- paste("https://github.com/b-steve/admbsecr/releases/download/v1.0.6/admbsecr_1.0.6-",
+                  os, ext, sep = "")
+dest <- paste("admbsecr_1.0.6", ext, sep = "")
 library(downloader)
-download("https://github.com/b-steve/admbsecr/releases/download/v1.0.6/admbsecr_1.0.6-win.zip", destfile = "admbsecr_1.0.6.zip")
-install.packages("admbsecr_1.0.6.zip", repos = NULL)
-unlink("admbsecr_1.0.6.zip")
+download(bin.name, destfile = dest)
+install.packages(dest, repos = NULL)
+unlink(dest)
