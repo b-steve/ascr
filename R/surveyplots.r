ba2cf08d0ba518ffc42af80a7af89013d24bca3c
Ben Stevenson <bcs5@st-andrews.ac.uk>
2015-04-29 17:01:54 +0100

    Adding trap locations to detection surface plot.

--- R/surveyplots.r
+++ R/surveyplots.r
@@ -22,18 +22,21 @@ show.survey <- function(fit, ...){
 #' \link{admbsecr}.
 #'
 #' @inheritParams locations
+#' @param ... Arguments to be passed to \link{plot}.
 #'
 #' @examples
 #' show.detsurf(example$fits$simple.hn)
 #'
 #' @export
-show.detsurf <- function(fit){
+show.detsurf <- function(fit, ...){
     p.det <- p.dot(fit)
     mask <- get.mask(fit)
+    traps <- get.traps(fit)
     unique.x <- sort(unique(mask[, 1]))
     unique.y <- sort(unique(mask[, 2]))
     z <- matrix(NA, nrow = length(unique.x), ncol = length(unique.y))
     n.mask <- nrow(mask)
+    n.traps <- nrow(traps)
     for (i in 1:n.mask){
         x <- mask[i, 1]
         y <- mask[i, 2]
@@ -41,14 +44,111 @@ show.detsurf <- function(fit){
         index.y <- which(y == unique.y)
         z[index.x, index.y] <- p.det[i]
     }
-    print(wireframe(z, zlim = c(0, 1),
-                    zlab = list("Detection probability", rot = 95),
-                    xlab = "x", ylab = "y", shade = TRUE,
-                    shade.colors.palette = palette.fun))
-    invisible(TRUE)
+    perspmat <- persp(x = unique.x, y = unique.y, z = z, zlim = c(0, 1),
+                      zlab = list("Detection probability", rot = 95),
+                      xlab = "", ylab = "", shade = 0.75, col = "lightblue",
+                      theta = 30, phi = 30, border = NA, ...)
+    for (i in 1:n.traps){
+        ds <- distances(traps[i, , drop = FALSE], mask)
+        nearest.mpoint <- which(ds == min(ds))[1]
+        nearest.z <- p.det[nearest.mpoint]
+        points(trans3d(x = traps[i, 1], y = traps[i, 2], z = nearest.z,
+                       pmat = perspmat), pch = 16, col = "red")
+    }
 }
 
-palette.fun <- function(irr, ref, height, saturation = 0.9){
-    hsv(h = 0.65, s = 1 - saturation * (1 - (1 - ref)^0.5), 
-        v = irr)
+#' Testing the mask object for a first calls model.
+#'
+#' Creates a diagnostic plot that can be used to test the adequacy of
+#' a mask for a first calls model.
+#'
+#' @inheritParams admbsecr
+#' @inheritParams locations
+#' @param pars A named list. Component names are parameter names, and
+#' components are values of parameters at which the mask is to be
+#' tested. Parameters must include \code{b0.ss}, \code{b1.ss}, and
+#' \code{sigma.ss}; see \link{admbsecr} for further details.
+#' @param cutoff The upper cutoff value; see \code{admbsecr} for
+#' further details.
+#' @param lower.cutoff The lower cutoff value; see \code{admbsecr} for
+#' further details.
+#' @param surface Logical, if \code{TRUE} a 3D detection surface is
+#' plotted over the mask point locations.
+#' @param ... Arguments to be passed to \link{plot}.
+#'
+#' @export
+mask.test <- function(fit = NULL, mask, traps, pars, cutoff, lower.cutoff, surface = FALSE, ...){
+    if (!is.null(fit)){
+        mask <- fit$args$mask
+        traps <- fit$args$traps
+        pars <- get.par(fit, pars = "fitted", as.list = TRUE)
+        cutoff <- fit$args$ss.opts$cutoff
+        lower.cutoff <- fit$args$ss.opts$lower.cutoff
+    }
+    traps.centroid <- matrix(apply(traps, 2, mean), nrow = 1)
+    ss.means <- pars$b0.ss - pars$b1.ss*distances(mask, traps)
+    log.p.au <- pnorm(cutoff, ss.means, pars$sigma.ss, lower.tail = FALSE, log.p = TRUE)
+    log.p.al <- pnorm(lower.cutoff, ss.means, pars$sigma.ss, lower.tail = FALSE, log.p = TRUE)
+    log.p.bu <- pnorm(cutoff, ss.means, pars$sigma.ss, lower.tail = TRUE, log.p = TRUE)
+    log.p.bl <- pnorm(lower.cutoff, ss.means, pars$sigma.ss, lower.tail = TRUE, log.p = TRUE)
+    n.mask <- nrow(mask)
+    n.traps <- nrow(traps)
+    n.combins <- 2^n.traps
+    combins <- matrix(NA, nrow = n.combins, ncol = n.traps)
+    for (i in 1:n.traps){
+        combins[, i] <- rep(rep(c(0, 1), each = 2^(n.traps - i)), times = 2^(i - 1))
+    }
+    p <- numeric(n.mask)
+    s <- numeric(n.mask)
+    for (i in 1:n.mask){
+        ## Log probabilities of detection and evasion at upper threshold.
+        log.det.u.mat <- matrix(log.p.au[i, ], nrow = n.combins, ncol = n.traps, byrow = TRUE)
+        log.evade.u.mat <- matrix(log.p.bu[i, ], nrow = n.combins, ncol = n.traps, byrow = TRUE)
+        log.prob.u.mat <- log.det.u.mat
+        log.prob.u.mat[combins == 0] <- log.evade.u.mat[combins == 0]
+        ## Probabilities for each capture history.
+        log.AU <- log(sum(exp(apply(log.prob.u.mat[-1, ], 1, sum))))
+        log.BU <- sum(log.prob.u.mat[1, ])
+        ## Log probabilities of detection and evasion at lower threshold.
+        log.det.l.mat <- matrix(log.p.al[i, ], nrow = n.combins, ncol = n.traps, byrow = TRUE)
+        log.evade.l.mat <- matrix(log.p.bl[i, ], nrow = n.combins, ncol = n.traps, byrow = TRUE)
+        log.prob.l.mat <- log.det.l.mat
+        log.prob.l.mat[combins == 0] <- log.evade.l.mat[combins == 0]
+        ## Probabilities for each capture history.
+        log.AL <- log(sum(exp(apply(log.prob.l.mat[-1, ], 1, sum))))
+        log.BL <- sum(log.prob.l.mat[1, ])
+        p[i] <- exp(log.AU)
+        s[i] <- exp(log.AU + log.BL - log.AL)
+    }
+    if (surface){
+        unique.x <- sort(unique(mask[, 1]))
+        unique.y <- sort(unique(mask[, 2]))
+        z <- matrix(NA, nrow = length(unique.x), ncol = length(unique.y))
+        n.mask <- nrow(mask)
+        for (i in 1:n.mask){
+            x <- mask[i, 1]
+            y <- mask[i, 2]
+            index.x <- which(x == unique.x)
+            index.y <- which(y == unique.y)
+            z[index.x, index.y] <- s[i]
+        }
+        perspmat <- persp(x = unique.x, y = unique.y, z = z, zlim = c(0, 1),
+                          zlab = list("", rot = 95),
+                          xlab = "", ylab = "", shade = 0.75, col = "lightblue",
+                          theta = 30, phi = 30, border = NA, ...)
+        for (i in 1:n.traps){
+            lines(trans3d(x = rep(traps[i, 1], 2), y = rep(traps[i, 2], 2), z = c(0, 0.1),
+                          pmat = perspmat))
+            points(trans3d(x = traps[i, 1], y = traps[i, 2], z = 0.1,
+                          pmat = perspmat), pch = 16, col = "red", cex = 0.5)
+        }
+    } else {
+        plot(distances(mask, traps.centroid), s + p, type = "n", ylim = c(0, 1), ...)
+        abline(h = c(0, 1), col = "lightgrey")
+        points(distances(mask, traps.centroid), s, col = "red", cex = 0.5)
+        points(distances(mask, traps.centroid), p, col = "blue", cex = 0.5)
+        points(distances(mask, traps.centroid), s + p, cex = 0.5)
+    }
 }
+
+
