ea82072dc7153c854c535a59a3ff3173f32f51d2
Ben Stevenson <bcs5@st-andrews.ac.uk>
2016-04-29 12:22:33 +0100

    Adding test for new algorithm.

--- inst/tests/test-convert.r
+++ inst/tests/test-convert.r
@@ -5,8 +5,14 @@ context("Testing conversion functions")
 test_that("Converting/creating capture histories", {
     dets <- lightfooti$dets
     traps <- lightfooti$traps
-    capt.test <- convert.pamguard(dets = dets, mics = traps)
+    ## Testing old allocation method.
+    capt.test <- convert.pamguard(dets = dets, mics = traps, new.allocation = FALSE)
+    ## New convert.pamguard() retains original time data.
+    capt.test$toa[capt.test$toa > 0] <- capt.test$toa[capt.test$toa > 0] - (capt.test$toa[1, 2] - 1)
     expect_that(capt.test, is_identical_to(lightfooti$capt))
+    ## Testing new allocation method.
+    capt.test.new <- convert.pamguard(dets = dets, mics = traps)
+    expect_that(capt.test.new, is_identical_to(lightfooti$capt.new))
     mask.test <- create.mask(traps = traps, buffer = 40)
     expect_that(mask.test, is_identical_to(lightfooti$mask))
 })
