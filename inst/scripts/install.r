98129330972869c02a342cab99905f44dae1382b
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2022-03-11 15:07:54 +1300

    Version bump.

--- inst/scripts/install.r
+++ inst/scripts/install.r
@@ -1,4 +1,4 @@
-pkgs <- c("CircStats", "fastGHQuad", "fields", "knitr", "matrixStats", "mgcv", "mvtnorm", "optimx", "plyr",
+2.2pkgs <- c("CircStats", "fastGHQuad", "fields", "knitr", "matrixStats", "mgcv", "mvtnorm", "optimx", "plyr",
           "Rcpp", "R2admb", "secr", "testthat", "truncnorm", "viridis", "xtable", "downloader")
 options(warn = -1)
 for (i in pkgs){
@@ -8,17 +8,17 @@ for (i in pkgs){
 }
 options(warn = 0)
 if (.Platform$OS == "windows"){
-    bin.name <- "https://github.com/b-steve/ascr/releases/download/v2.2.3/ascr_2.2.3.zip"
+    bin.name <- "https://github.com/b-steve/ascr/releases/download/v2.2.4/ascr_2.2.4.zip"
     ext <- ".zip"
     type <- "win.binary"
 } else if (.Platform$OS == "unix"){
-    bin.name <- "https://github.com/b-steve/ascr/archive/v2.2.3.tar.gz"
+    bin.name <- "https://github.com/b-steve/ascr/archive/v2.2.4.tar.gz"
     ext <- ".tar.gz"
     type <- "source"
 } else {
     stop("Unknown OS type.")
 }
-dest <- paste("ascr_2.2.3", ext, sep = "")
+dest <- paste("ascr_2.2.4", ext, sep = "")
 library(downloader)
 download(bin.name, destfile = dest)
 install.packages(dest, repos = NULL, type = type)
